4fa17583e6e57d7c4294d294a467030b1e5513f7
Солонщиков Артём Александрович <solonshchikov.a.a@sberbank.ru>
2021-06-07 18:48:36 +0300

    Test Apache HttpClient

---
+++ src/main/java/com/example/testall/httpclient/HttpClientTest.java
@@ -0,0 +1,73 @@
+package com.example.testall.httpclient;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+public class HttpClientTest {
+
+    public static void main(String[] args) throws InterruptedException {
+        CloseableHttpClient client = HttpClientBuilder
+                .create()
+                .setDefaultRequestConfig(
+                        RequestConfig.custom()
+                                .setConnectTimeout(60 * 1000)
+                                .setConnectionRequestTimeout(30 * 1000)
+                                .setSocketTimeout(60 * 1000)
+                                .build()
+                )
+                .setMaxConnPerRoute(2)
+                .setMaxConnTotal(2)
+//                .setConnectionTimeToLive(5, TimeUnit.SECONDS) // время жизни соединения в пуле, контролируемое исключительно на стороне клиента
+                .evictIdleConnections(5, TimeUnit.SECONDS) // если соединение простаивало какое-то время (пусть даже и не закрыто сервером), то оно будет закрыто
+                .evictExpiredConnections() // если соединение было разорвано на стороне сервера, то оно будет закрыто (при этом время по умолчанию 10 сек или же берется из evictIdleConnections),
+                                           // при этом простаивающие соединения не афектятся
+                // PoolingHttpClientConnectionManager.setValidateAfterInactivity раньше перед выдачей соединения из пула оно всегда проверялось,
+                                                                              // а теперь оно проверяется только если прошло больше указанного кол-ва  ms
+                .build();
+
+        HttpGet get1 = new HttpGet("http://localhost:8081/hello");
+        HttpGet get2 = new HttpGet("http://localhost:8081/hello");
+        HttpGet get3 = new HttpGet("http://localhost:8081/hello");
+
+        MultiHttpClientConnThread thread1 = new MultiHttpClientConnThread(client, get1);
+        MultiHttpClientConnThread thread2 = new MultiHttpClientConnThread(client, get2);
+        MultiHttpClientConnThread thread3 = new MultiHttpClientConnThread(client, get3);
+        thread1.start();
+        thread2.start();
+        thread3.start();
+
+        thread1.join();
+        thread2.join();
+        thread3.join();
+    }
+
+    public static class MultiHttpClientConnThread extends Thread {
+        private CloseableHttpClient client;
+        private HttpGet get;
+
+        public MultiHttpClientConnThread(CloseableHttpClient client, HttpGet get) {
+            this.client = client;
+            this.get = get;
+        }
+
+        // standard constructors
+        public void run() {
+            try {
+                System.out.println(Thread.currentThread().getName() + " prepare make request");
+                HttpResponse response = client.execute(get);
+                System.out.println(Thread.currentThread().getName() + " made request");
+                Thread.sleep( 20 * 1000);
+                System.out.println(Thread.currentThread().getName() + " wake up");
+                EntityUtils.consume(response.getEntity());
+            } catch (IOException | InterruptedException ex) {
+            }
+        }
+    }
+}
--- src/main/java/com/example/testall/simplecontroller/SimpleController.java
+++ src/main/java/com/example/testall/simplecontroller/SimpleController.java
@@ -1,11 +1,37 @@
 package com.example.testall.simplecontroller;
 
+import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
 @RestController
 public class SimpleController {
+
+    private static final Logger log = LoggerFactory.getLogger(SimpleController.class);
+
+    CloseableHttpClient client = HttpClientBuilder
+            .create()
+            .setDefaultRequestConfig(
+                    RequestConfig.custom()
+                            .setConnectTimeout(60 * 1000)
+                            .setConnectionRequestTimeout(5 * 1000)
+                            .setSocketTimeout(60 * 1000)
+                            .build()
+            )
+            .setMaxConnPerRoute(2)
+            .setMaxConnTotal(2)
+            .build();
 
     @GetMapping("/hello")
     public String hello() {
@@ -16,5 +42,23 @@ public class SimpleController {
     @PostMapping("/ott-service/TokenJsonRpcFacade.json")
     public String ott(Object obj) {
         return "{\"jsonrpc\": \"2.0\", \"result\": 19, \"id\": 3}";
+    }
+
+    @GetMapping("/hello_2")
+    public String hello2() throws IOException {
+        log.info(" /hello_2 received request");
+
+        HttpGet get = new HttpGet("http://localhost:8081/hello_with_sleep");
+        HttpResponse response = client.execute(get);
+
+        return IOUtils.toString(response.getEntity().getContent(), StandardCharsets.UTF_8);
+    }
+
+    @GetMapping("/hello_with_sleep")
+    public String helloWithSleep() throws InterruptedException {
+        log.info(" /hello_with_sleep received request");
+        Thread.sleep(30 * 1000);
+        log.info(" /hello_with_sleep send response");
+        return "Heeello hi!";
     }
 }
